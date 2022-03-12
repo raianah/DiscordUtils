70ff0f1bcd3e088a86b48259ce70bc56ee99dd18
raianah <82108512+raianah@users.noreply.github.com>
2022-03-12 11:33:40 +0800

    renamed from discord to disnake for compatibility

--- DiscordUtils/__init__.py
+++ DiscordUtils/__init__.py
@@ -2,7 +2,7 @@ from DiscordUtils import Pagination
 from DiscordUtils.InviteTracker import InviteTracker
 from DiscordUtils.Music import Music
 
-__title__ = "DiscordUtils"
+__title__ = "DiscordUtils for Disnake"
 __version__ = "1.3.4"
-__author__ = "toxicrecker"
+__author__ = "toxicrecker (forked by raianah)"
 __license__ = "MIT"
